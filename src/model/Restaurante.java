296cabcf00748788ac80f0ed8e6244da50235d97
Carla Drieli <carladrieli@gmail.com>
2016-04-19 12:46:54 -0300

    criando projeto

---
+++ src/model/Restaurante.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author cmelo
+ */
+public class Restaurante {
+    private Integer id;
+    private String nome;
+    private String CNPJ;
+    
+    public Integer getId(){
+        return id;
+    }
+    
+    
+}
