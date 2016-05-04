e2db5d70e1b092364ea1946a32b1f985f02d8336
Igor Andrade <igorandrade@dcc.ufba.br>
2016-05-03 22:48:25 -0300

    Correção

---
+++ Conta.cc
@@ -0,0 +1,3 @@
+<idConta>1<idCliente>1<idMesa>2<dataHoraInicioAtendimento>03-05-2016 22:39:00<dataHoraFimAtendimento>null<fdl>
+<idConta>2<idCliente>1<idMesa>2<dataHoraInicioAtendimento>03-05-2016 22:40:00<dataHoraFimAtendimento>null<fdl>
+<idConta>3<idCliente>1<idMesa>2<dataHoraInicioAtendimento>03-05-2016 22:41:45<dataHoraFimAtendimento>03-05-2016 22:43:45<fdl>
--- Pedido.cc
+++ Pedido.cc
@@ -1,1 +1,2 @@
-
+<idPedido>1<conta>1<item>2<quantidade>2<dataHora>03-05-2016 22:39:21<garcom>null<cozinheiro>null<fdl>
+<idPedido>2<conta>3<item>2<quantidade>2<dataHora>03-05-2016 22:41:50<garcom>null<cozinheiro>null<fdl>
